e7301d09667c949e0c27dbc084a208f318af7983
Yaroslav <plehanov.kzn2@gmail.com>
2026-05-10 16:38:55 +0300

    генератор кода sql

--- internal/generator/sql.go
+++ internal/generator/sql.go
@@ -1,1 +1,38 @@
 package generator
+
+import (
+	"db-designer-vkr/internal/model"
+	"fmt"
+	"strings"
+)
+
+func GenerateSQL(entities []model.Entity) string {
+	var builder strings.Builder
+
+	for _, entity := range entities {
+		tableName := strings.ToLower(entity.Name)
+
+		builder.WriteString(
+			fmt.Sprintf(
+				"CREATE TABLE %s (\n",
+				tableName,
+			),
+		)
+
+		builder.WriteString("    id SERIAL PRIMARY KEY")
+
+		// атрибуты
+		for _, attr := range entity.Attributes {
+			builder.WriteString(
+				fmt.Sprintf(
+					",\n    %s TEXT",
+					attr.Name,
+				),
+			)
+		}
+
+		builder.WriteString("\n);\n\n")
+	}
+
+	return builder.String()
+}
